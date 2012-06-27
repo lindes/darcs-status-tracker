7d7c490bfbbb0c7deef9f73471d3870a5d83faad
David Lindes <misc-git-user@daveltd.com>
2012-06-27 19:10:26 +0200

    allow a name parameter for a config (w/ defaults)

--- darcs-status-tracker.lisp
+++ darcs-status-tracker.lisp
@@ -32,48 +32,76 @@
                                (parse-outputs :lisp "foo.lisp"))))
 
 ; turn path with optional host into '(path host)
-(defun canonical-config (path &optional host)
+(defun canonical-config (path &optional host name)
   (if (listp path)
       (apply #'canonical-config path)
-      (list path host)))
+      (let ((safe-path (substitute #\_ #\/ (remove #\~ path))))
+        (unless name
+          (setf name (if host
+                         (concatenate 'string host "-" safe-path)
+                         safe-path)))
+        (list path host name))))
 
-#+5am (5am:test test-canonical-config
+
+; #+5am
+(5am:test test-canonical-config
         "Testing canonical-config"
-        (5am:is (equal '("foo" nil)
-                   (canonical-config "foo")))
-        (5am:is (equal '("foo" "bar")
-                   (canonical-config "foo" "bar"))))
+        (5am:is (equal '("foo" nil "foo")
+                       (canonical-config "foo")))
+        (5am:is (equal '("foo" "bar" "bar-foo")
+                       (canonical-config "foo" "bar")))
+        (5am:is (equal '("foo" "bar" "baz")
+                       (canonical-config "foo" "bar" "baz"))))
 
 ; what I want as input:
-; (parse-configs "/dir" "/dir2" ("/dir3" "host"))
+; (parse-configs "~d/i/r" ; directory only
+;                "/dir2" ; another
+;               ("/dir3" "host") ; directory with (optional user@ and) host
+;               ("/dir4" "host2" "name-for-host2-dir-4") ; the same, with an explicit nickname
 ; what I want to get back:
-; (("/dir" nil) ("/dir2" nil) ("/dir3" "host"))
+; (
+;  ("~d/i/r" nil "d_i_r") ; nickname generated from path
+;  ("/dir2" nil "dir2")   ; same
+;  ("/dir3" "host" "host-dir3") ; nickname generated from host and path
+;  ("/dir4" "host2" "name-for-host2-dir-4") ; as passed
+; )
 ;
 ; well, but really, this doesn't need to be exposed directly, and it's
 ; fine if it just gets a single list, with the caller dealing with
 ; changing it from looking like the above to looking like:
-; (parse-configs "/dir" "/dir2" '("/dir3" "host")))
+; (parse-configs "/dir" "/dir2" '("/dir3" "host"))
 (defun parse-configs (&rest configs)
   (loop for c in configs collecting (canonical-config c)))
 
 #+5am (5am:test test-parse-configs
         "Testing the way parse-configs splits things up"
-        (5am:is (equal '(("foo" nil) ("foo" "bar"))
-                       (parse-configs "foo" '("foo" "bar")))))
+        (5am:is (equal '(("foo/bar" nil "foo_bar") ("foo" "bar" "bar-foo"))
+                       (parse-configs "foo/bar" '("foo" "bar")))))
 
 ; outputs would-be: &key html text lisp
 
-;
+; execute a configuration
+(defun fetch-data-for-config (config)
+  (let ((path (car config))
+        (host (cadr config))
+        (name (caddr config)))
+    (let ((command (append (when host (list "ssh" host))
+                           (list "darcs" "log" "--repodir" path "--xml-output"))))
+      (format t "running ~a: ~s~%" name command)
+      (with-open-file (out (concatenate 'string name ".xml")
+                           :direction :output)
+        (sb-ext:run-program (first command) (rest command)
+                            :search t
+                            :output out
+                            :error t)))))
+
+; the main loop:
 (defun _darcs-status-tracker (outputs configs)
   ; (apply #'parse-outputs outputs)
-  ; (format t "outputs: ~S, conf: ~S~%" outputs configs)
+  (format t "outputs: ~S, conf: ~S~%" outputs configs)
 
   (loop for config in configs
-     do (let ((path (car config))
-              (host (cadr config)))
-          (if host
-              (format t "running on ~a for ~a~%" host path)
-              (format t "running locally for ~a~%" path)))))
+     do (fetch-data-for-config config)))
 
 ; this allows us to have configs in a somewhat informal style, that
 ; just look like lisp-forms without need for quoting them.
